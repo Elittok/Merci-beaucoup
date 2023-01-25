a85ede1ebb7b76cd365ac0c191dfff8d820e0750
ZXUI!G <76615554+Elittok@users.noreply.github.com>
2023-01-25 11:32:09 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -68,3 +68,9 @@ def f9():
         if y>=len(result):break         
         x=x+6
         y=x+6
+def f 14():
+    a = ("0123456789abcde")
+    for x in a:
+        f = int(f'123{x}5',15) + int(f'1{x}233',15)
+        if f%14==0:
+            print(x,f//14)
